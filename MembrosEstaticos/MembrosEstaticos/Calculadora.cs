d24964dec89679c198e7bd221fc1af15f4efa89a
LeoMartinsBDS <34400625+LeoMartinsBDS@users.noreply.github.com>
2019-03-03 20:00:53 -0300

    membros estáticos

---
+++ MembrosEstaticos/MembrosEstaticos/Calculadora.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MembrosEstaticos
+{
+    class Calculadora
+    {
+        //static permite a chamada sem a instancia do objeto
+        public static double Pi = 3.14;
+
+        public static double Circunferencia(double r)
+        {
+            return 2 * Pi * r;
+        }
+
+        public static double Volume(double r)
+        {
+            return 4.0 / 3.0 * Pi * Math.Pow(r, 3.0);
+        }
+    }
+}
---
+++ MembrosEstaticos/MembrosEstaticos/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace MembrosEstaticos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Entre o valor do raio: ");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double circ = Calculadora.Circunferencia(raio);
+            double vol = Calculadora.Volume(raio);
+
+            Console.WriteLine("Circunferencia: " + circ.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Raio: " + vol.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor de Pi: " + Calculadora.Pi.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
