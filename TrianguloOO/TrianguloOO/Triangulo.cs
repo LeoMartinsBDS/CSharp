5c047cefb0b96479491705c2fede5ae3fecfaee9
LeoMartinsBDS <34400625+LeoMartinsBDS@users.noreply.github.com>
2019-03-03 15:49:48 -0300

    adicionado método de calcula de area

--- TrianguloOO/TrianguloOO/Triangulo.cs
+++ TrianguloOO/TrianguloOO/Triangulo.cs
@@ -1,10 +1,22 @@
-﻿namespace TrianguloOO
+﻿using System;
+
+namespace TrianguloOO
 {
     class Triangulo
     {
         public double A { get; set; }
         public double B { get; set; }
         public double C { get; set; }
+        
+
+        public double calcularArea(double A, double B, double C)
+        {
+            double p = (A + B + C) / 2.0;
+
+            double area = Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+
+            return area;
+        }
 
     }
 
