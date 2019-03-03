1b8da6db6143204f0005ace607121976904e8b55
LeoMartinsBDS <34400625+LeoMartinsBDS@users.noreply.github.com>
2019-03-03 19:35:08 -0300

    Estudo orientação objeto

---
+++ Estoque/Estoque/Produto.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Estoque
+{
+    class Produto
+    {
+        public string Nome { get; set; }
+
+        public double Preco { get; set; }
+
+        public int QuantidadeEstoque { get; set; }
+
+        public double ValorTotalEstoque()
+        {
+            return Preco * QuantidadeEstoque;
+        }
+
+        public void AdicionarProdutoEstoque(int qtdDigitada)
+        {
+            QuantidadeEstoque += qtdDigitada;
+        }
+
+        public void RemoverProdutoEstoque(int qtdDigitada)
+        {
+            QuantidadeEstoque -= qtdDigitada;
+        }
+
+
+        public override string ToString()
+        {
+            return Nome
+                   + ", R$ " + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                   + ", " + QuantidadeEstoque + " unidades, Total: R$"
+                   + ValorTotalEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
