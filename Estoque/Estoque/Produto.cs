02569e41130049f296faff3dcfab981c90912d2e
LeoMartinsBDS <34400625+LeoMartinsBDS@users.noreply.github.com>
2019-03-04 15:44:50 -0300

    this, encapsulamente, properties

--- Estoque/Estoque/Produto.cs
+++ Estoque/Estoque/Produto.cs
@@ -7,17 +7,42 @@ namespace Estoque
 {
     class Produto
     {
-        public string Nome { get; set; }
+        //atributo private segue esse padrao
+        private string _nome;
 
-        public double Preco { get; set; }
+        public double Preco { get; private set; }
+        public int QuantidadeEstoque { get; private set; }
 
-        public int QuantidadeEstoque { get; set; }
-
-        public Produto(string nome, double preco, int qtd) {
-            Nome = nome;
+        public Produto()
+        {
+            QuantidadeEstoque = 10;
+        }
+        //SOBRECARGA
+        //this utilizado para referenciar o construtor
+        public Produto(string nome, double preco) : this()
+        {
+            _nome = nome;
             Preco = preco;
+        }
+
+        public Produto(string nome, double preco, int qtd) : this(nome, preco)
+        {
             QuantidadeEstoque = qtd;
         }
+
+        public string GetNome()
+        {
+            return _nome;
+        }
+
+        public void SetNome(string nome)
+        {
+            if (nome != null && nome.Length > 1)
+            {
+                _nome = nome;
+            }
+        }
+
 
         public double ValorTotalEstoque()
         {
@@ -37,7 +62,7 @@ namespace Estoque
 
         public override string ToString()
         {
-            return Nome
+            return _nome
                    + ", R$ " + Preco.ToString("F2", CultureInfo.InvariantCulture)
                    + ", " + QuantidadeEstoque + " unidades, Total: R$"
                    + ValorTotalEstoque().ToString("F2", CultureInfo.InvariantCulture);
--- Estoque/Estoque/Program.cs
+++ Estoque/Estoque/Program.cs
@@ -6,42 +6,15 @@ namespace Estoque
     class Program
     {
         static void Main(string[] args)
-        {   
-            Console.WriteLine("Entre os dados do produto");
+        {
+            Produto p = new Produto("TV", 500.00, 10);
 
-            Console.Write("Nome: ");
-            string nome = Console.ReadLine();
+            //É IGNORADO PELA VALIDAÇÃO
+            p.SetNome("T");
 
-            Console.Write("Preço: ");
-            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-            Console.Write("Quantidade no estoque: ");
-            int qtd = int.Parse(Console.ReadLine());
-
-
-            Produto p = new Produto(nome, preco, qtd);
-
-            Console.WriteLine();
-            Console.WriteLine("Dados do produto: " + p);
-            Console.WriteLine();
-
-            Console.Write("Digite o número de produtos a serem adicionados no estoque: ");
-            int qte = int.Parse(Console.ReadLine());
-
-            p.AdicionarProdutoEstoque(qte);
-
-            Console.WriteLine();
-            Console.WriteLine("Dados atualizados: " + p);
-            Console.WriteLine();
-
-            Console.Write("Digite o número de produtos a serem removidos no estoque: ");
-            qte = int.Parse(Console.ReadLine());
-
-            p.RemoverProdutoEstoque(qte);
-
-            Console.WriteLine();
-            Console.WriteLine("Dados atualizados: " + p);
-            Console.WriteLine();
+            Console.WriteLine(p.GetNome());
+            Console.WriteLine(p.Preco);
+            Console.WriteLine(p.QuantidadeEstoque);
 
         }
     }
