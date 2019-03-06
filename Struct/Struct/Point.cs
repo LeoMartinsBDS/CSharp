f0b6bd754a7ae6bcbfab0f4787f01297b08963a0
LeoMartinsBDS <34400625+LeoMartinsBDS@users.noreply.github.com>
2019-03-06 12:14:19 -0300

    STRUCT, NULLABLE E COALESCENCIA NULA

---
+++ Struct/Struct/Point.cs
@@ -0,0 +1,14 @@
+﻿namespace Struct
+{
+    struct Point
+    {
+        public double x;
+        public double y;
+
+        public override string ToString()
+        {
+            return "(" + x + ", " + y + ")"; 
+        }
+
+    }
+}
---
+++ Struct/Struct/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Struct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("STRUCT");
+            Point p;
+
+            p.x = 10;
+            p.y = 20;
+            Console.WriteLine(p);
+
+            //se inicializar ele pega o valor padrão
+            p = new Point();
+            Console.WriteLine(p);
+
+            //variavel declarada mas não instanciada fica como nao atribuida
+            //Classe -> variaveis são ponteiros, OO
+            //Struct -> variaveis são caixas, perfomático
+
+            //NULLABLE
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("NULLABLE");
+            double? x = null;
+            double? y = 10.0;
+
+            Console.WriteLine(x.GetValueOrDefault());
+            Console.WriteLine(y.GetValueOrDefault());
+
+            Console.WriteLine(x.HasValue);
+            Console.WriteLine(y.HasValue);
+
+            if(x.HasValue)
+                Console.WriteLine(x.Value);
+            else
+                Console.WriteLine("X is null");
+
+            if(y.HasValue)
+                Console.WriteLine(y.Value);
+            else
+                Console.WriteLine("Y is null");
+
+
+            //OPERADOR DE COALESCENCIA NULA
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("COALESCENCIA NULA");
+
+            double a = x ?? 5;
+            double b = y ?? 5;
+
+            Console.WriteLine("A: "+ a);
+            Console.WriteLine("B: "+ b);
+        }
+    }
+}
