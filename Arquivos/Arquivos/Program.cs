61a28e167dbf05cae24b142514aa9265ef733c46
LeoMartinsBDS <34400625+LeoMartinsBDS@users.noreply.github.com>
2019-03-19 16:01:14 -0300

    arquivos

---
+++ Arquivos/Arquivos/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.IO;
+
+namespace Arquivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //@ faz com que tenha que digitar \ ao inves de \\
+            string sourcePath = @"c:\temp";
+            string sourcePath2 = @"c:\temp\file1.txt";
+            //string targetPath = @"c:\temp\file1.txt";
+
+            Console.WriteLine("Directory name: " + Path.GetDirectoryName(sourcePath2));
+
+
+            try
+            {
+                //list directories
+                var folders = Directory.EnumerateDirectories(sourcePath, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FOLDERS: ");
+                foreach (string s in folders) {
+                    Console.WriteLine(s);                }
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+
+            try
+            {
+                //list files
+                var files = Directory.EnumerateFiles(sourcePath, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FILES: ");
+                foreach (string s in files)
+                {
+                    Console.WriteLine(s);
+                }
+
+                //create directory
+                Directory.CreateDirectory(sourcePath + @"\memes");
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+
+            //WRITE
+            /*try
+            {
+                string[] lines = File.ReadAllLines(sourcePath);
+
+                using (StreamWriter sw = File.AppendText(targetPath))
+                {
+                    foreach (string line in lines)
+                    {
+                        sw.WriteLine(line.ToUpper());
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+            /*
+
+            //READ
+            //FECHA AS OPERAÇÕES AUTOMATICAMENTE
+            /*try
+            {
+                using (StreamReader sr = File.OpenText(sourcePath))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }*/
+
+            /*
+            try
+            {
+                sr = File.OpenText(sourcePath);
+
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+
+                    Console.WriteLine(line);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (sr != null)
+                    sr.Close();
+            }
+            */
+
+            /*try
+            {
+                FileInfo fileInfo = new FileInfo(sourcePath);
+                fileInfo.CopyTo(targetPath);
+
+                string[] lines = File.ReadAllLines(sourcePath);
+
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }*/
+        }
+    }
+}
