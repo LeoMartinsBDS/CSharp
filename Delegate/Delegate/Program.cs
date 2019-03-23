7e139ce71dc62e9250547ce714235319019b221b
LeoMartinsBDS <34400625+LeoMartinsBDS@users.noreply.github.com>
2019-03-22 21:00:52 -0300

    Expressões lambda, delegates, LINQ

---
+++ Delegate/Delegate/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Delegate.Services;
+
+namespace Delegate
+{
+    //declaração do delegate
+    delegate void BinaryNumericOperation(double n1, double n2);
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+            //op é uma referencia a função soma
+            //multicast delegate
+            BinaryNumericOperation op = CalculationService.ShowSum;
+            op += CalculationService.ShowMax;
+
+            op.Invoke(a,b);
+        }
+    }
+}
---
+++ Predicate/Predicate/Program.cs
@@ -0,0 +1,64 @@
+﻿using Predicate.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Predicate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("TV", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
+
+            //delegate para referencia a uma função
+            Func<Product, string> func = NameUpper;
+
+            List<string> result = list.Select(func).ToList();
+            foreach (string s in result)
+            {
+                Console.WriteLine(s);
+            }
+            Console.WriteLine("");
+
+            //action
+            Action<Product> act = p => { p.Price += p.Price * 0.1; };
+
+            list.ForEach(act);
+            foreach (Product prod in list)
+            {
+                Console.WriteLine(prod);
+            }
+
+            Console.WriteLine("Removido");
+            list.RemoveAll(ProductTeste);
+
+            foreach (Product prod in list)
+            {
+                Console.WriteLine(prod);
+            }
+        }
+
+        static string NameUpper(Product p)
+        {
+            return p.Name.ToUpper();
+        }
+
+        static void UpdatePrice(Product p)
+        {
+            p.Price += p.Price * 0.1;
+        }
+
+        //PREDICATE
+        public static bool ProductTeste(Product p)
+        {
+            return p.Price >= 100;
+        }
+
+    }
+}
